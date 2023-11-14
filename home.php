1cb785868e4c15c3660fd0353364160d991cee18
TERATERA-b-a-i-t-o <135669741+TERATERA-b-a-i-t-o@users.noreply.github.com>
2023-11-14 11:03:13 -0300

    aymiproyecytogod

--- home.php
+++ home.php
@@ -3,63 +3,121 @@
 
 <head>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-    <link rel="stylesheet" type="text/css" href="styleB.css">
+    <link rel="stylesheet" type="text/css" href="styleb.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SCRIMER</title>
 </head>
 
-<body>
-    <div class="contenedor">
+<body data-barba="wrapper">
+    <div data-barba="container" data-barba-namespace="">
 
-        <div class="fondo">
-            <img src="images/mario.gif" alt="" width="900">
-        </div>
-        <div class="rectangle-4">
-            <button class="rectangle-6" onclick="press()"><span id="dot" class="material-symbols-outlined">
-                    more_vert
-                </span></button>
+        <div class="contenedor">
 
-            <div class="rectangle-7" id="barra">
-                <button class="bb" onclick="getback()"></button>
-                <span class="material-symbols-outlined" id="baku">
-                    reply
-                </span>
 
+            <img class="det" src="images/pixelh.png" alt="">
+            <img class="det" src="images/star.png" alt="">
+            <div class="fondo">
+                <img src="images/mario.gif" alt="" width="900">
             </div>
 
-            <input class="rectangle-5" type="text" name="buscador" placeholder="Buscar">
+            <div class="rectangle-4">
+                <a class="linku" href="../login/login.php" rel="noopener noreferrer">inicia sesion</a>
+                <button class="rectangle-6" onclick="press()"><span id="dot" class="material-symbols-outlined">
+                        more_vert
+                    </span></button>
 
-            <h5 class="usuario">usuario de ejemplo</h5>
-            <div class="account-circle-icon-1"><span " class=" material-symbols-outlined">
-                    account_circle
+
+                <div class="rectangle-7" id="barra">
+                    <button class="bb" onclick="getback()"></button>
+                    <span class="material-symbols-outlined" id="baku">
+                        reply
+                    </span>
+
+                    <div class="selector">
+                        <h5>
+                            HOME<a href="../home/home.php"></a><span id="obj" class="material-symbols-outlined">
+                                home
+                            </span>
+                        </h5>
+
+                        <h5>
+                            videojuegos<a href="#"></a><span id="obj1" class="material-symbols-outlined">
+                                stadia_controller
+                            </span>
+                        </h5>
+
+                        <h5>
+                            peliculas<a href="#"></a><span id="obj2" class="material-symbols-outlined">
+                                theaters
+                            </span>
+                        </h5>
+
+                        <h5>
+                            tendencias<a href="#"></a><span id="obj3" class="material-symbols-outlined">
+                                trending_up
+                            </span>
+                        </h5>
+
+                        <h5>
+                            ranking<a href="#"></a><span id="obj" class="material-symbols-outlined">
+                                star_half
+                            </span>
+                        </h5>
+                    </div>
+                    <div class="opciones">
+                        <h5>
+                            configuracion<a href="#"></a><span id="obj4" class="material-symbols-outlined">
+                                settings
+                            </span>
+                        </h5>
+
+                        <h5>
+                            personalizacion<a href="#"></a><span id="obj5" class="material-symbols-outlined">
+                                construction
+                            </span>
+                        </h5>
+                    </div>
+
+                </div>
+
+                <input class="rectangle-5" type="text" name="buscador" placeholder="Buscar">
+
+                <h5 class="usuario">usuario de ejemplo</h5>
+                <div class="account-circle-icon-1"><span " class=" material-symbols-outlined">
+                        account_circle
+                    </span></div>
+            </div>
+
+            <div class="titulo">
+                <h3>SCRIMER</h3>
+            </div>
+            <div class="next"> <span class="material-symbols-outlined">
+                    navigate_next
                 </span></div>
-        </div>
-        
-        <div class="titulo">
-            <h3>SCRIMER</h3>
-        </div>
 
 
+        </div>
     </div>
 </body>
+<!--<script type="text/javascript" src="js/barba.min.js"></script>
+<script type="text/javascript" src="js/custom.js"></script> !-->
 <script>
-    var a = 1;
+    //var a = 1;
 
     function press() {
 
-        a++;
-        console.log(a);
+        //a++;
+        //console.log(a);
         document.querySelector("#barra").style.animation = "movesin 0.8s";
         document.querySelector("#barra").style.transform = "translateX(0px)"
     }
 
     function getback() {
-        a++;
-        console.log(a);
+        //a++;
+        //console.log(a);
         document.querySelector("#barra").style.animation = "movesout 0.8s";
         document.querySelector("#barra").style.transform = "translateX(-300px)";
-
     }
 </script>
 
